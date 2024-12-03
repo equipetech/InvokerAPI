5d91df65e64b3d3705c81c61d630eef6d1be5dee
Rodrigo.Luparelli <rodrigo.luparelli@globalhitss.com.br>
2024-12-02 23:46:38 -0300

    fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,4 @@
 import { DataSource } from 'typeorm';
-import { UserEntity } from './infrastructure/database/entities/userEntity';
 import dotenv from 'dotenv';
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
